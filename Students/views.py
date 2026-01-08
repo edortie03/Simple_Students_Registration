a5f3ebfe5c309169555894a90d8248ed641d17dd
happnesnyori <happnesnyori@gmail.com>
2026-01-08 23:08:07 +0300

    Adding a collection

--- Students/views.py
+++ Students/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Student
-from .forms import StudentForm
+from django.forms import StudentForm
 
 # Register Student
 def register_student(request):
     if request.method == 'POST':
-        form = StudentForm(request.POST)
+        form = StudentForm(request.
         if form.is_valid():
             form.save()
             return redirect('home')
