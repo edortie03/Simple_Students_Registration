c16261b7e42ddf26f9f8457449c17d7ae822fd89
happnesnyori <happnesnyori@gmail.com>
2026-01-08 22:44:08 +0300

    Uploading my project

--- Students/views.py
+++ Students/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Student
+from .forms import StudentForm
 
-# Create your views here.
+# Register Student
+def register_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = StudentForm()
+    return render(request, 'Students/register.html', {'form': form})
